fe3831be8d43140ee89c23a6f3e6fb01eca3a567
Martyna Kiljan <kmartynak@icloud.com>
2021-03-20 14:51:16 +0100

    add Route, add Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,25 @@
-import Page from './Components/Page'
+
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import {BrowserRouter as Router, Route, Switch} from 'react-dom'
+import Home from './Components/Home'
+import Contact from './Components/Contact'
+import noMatch from './Components/Nomatch'
+import Aboutus from './Components/Aboutus'
+
+
 
 function App() {
   return (
     <>
-    <Page/>
+    <Route>
+      <Switch>
+        <Route  exact path="/" component={Home}></Route>
+        <Route  path="/aboutuse" component={Aboutus}></Route>
+        <Route  path="/contact" component={Contact}></Route>
+        <Route  component={noMatch}></Route>
+      </Switch>
+    </Route>
 
     </>
   );
